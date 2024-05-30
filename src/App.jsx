1ffbaab942ce6247bda4e2fa239ec09fde2cc25f
Rupesh Thakur <rupeshthakur80078@gmail.com>
2024-05-30 08:17:55 +0530

    test-1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Calculator from "./components/Pages/Calculator";
 function App() {
   return (
     <div className="min-h-screen bg-gray-200 flex justify-center items-center">
-      <div className="h-[90%] w-screen sm:rounded-3xl sm:shadow-md flex flex-col">
+      <div className="sm:rounded-3xl sm:shadow-md flex flex-col">
         <Calculator />
       </div>
     </div>
